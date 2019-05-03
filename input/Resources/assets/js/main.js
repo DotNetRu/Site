03c45e022562b67bc2846ae7ef75415aab75e8a9
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2019-05-03 23:22:12 +0300

    Update HTML5 UP template

--- input/Resources/assets/js/main.js
+++ input/Resources/assets/js/main.js
@@ -6,161 +6,140 @@
 
 (function($) {
 
-	"use strict";
+	var	$window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+	// Breakpoints.
+		breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
 
-	$(function() {
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+			$window.on('resize', function() { $window.trigger('scroll'); });
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
+			$banner.scrollex({
+				bottom:		$header.outerHeight(),
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
 			});
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		}
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+	// Menu.
+		var $menu = $('#menu');
 
-		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
+		$menu._locked = false;
 
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
+		$menu._lock = function() {
 
-				$window.on('resize', function() { $window.trigger('scroll'); });
+			if ($menu._locked)
+				return false;
 
-				$banner.scrollex({
-					bottom:		$header.outerHeight(),
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
+			$menu._locked = true;
 
-			}
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
 
-		// Menu.
-			var $menu = $('#menu');
+			return true;
 
-			$menu._locked = false;
+		};
 
-			$menu._lock = function() {
+		$menu._show = function() {
 
-				if ($menu._locked)
-					return false;
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
 
-				$menu._locked = true;
+		};
 
-				window.setTimeout(function() {
-					$menu._locked = false;
-				}, 350);
+		$menu._hide = function() {
 
-				return true;
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
 
-			};
+		};
 
-			$menu._show = function() {
+		$menu._toggle = function() {
 
-				if ($menu._lock())
-					$body.addClass('is-menu-visible');
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
 
-			};
+		};
 
-			$menu._hide = function() {
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
 
-				if ($menu._lock())
-					$body.removeClass('is-menu-visible');
+				event.stopPropagation();
 
-			};
+				// Hide.
+					$menu._hide();
 
-			$menu._toggle = function() {
+			})
+			.find('.inner')
+				.on('click', '.close', function(event) {
 
-				if ($menu._lock())
-					$body.toggleClass('is-menu-visible');
-
-			};
-
-			$menu
-				.appendTo($body)
-				.on('click', function(event) {
-
+					event.preventDefault();
 					event.stopPropagation();
+					event.stopImmediatePropagation();
 
 					// Hide.
 						$menu._hide();
 
 				})
-				.find('.inner')
-					.on('click', '.close', function(event) {
-
-						event.preventDefault();
-						event.stopPropagation();
-						event.stopImmediatePropagation();
-
-						// Hide.
-							$menu._hide();
-
-					})
-					.on('click', function(event) {
-						event.stopPropagation();
-					})
-					.on('click', 'a', function(event) {
-
-						var href = $(this).attr('href');
-
-						event.preventDefault();
-						event.stopPropagation();
-
-						// Hide.
-							$menu._hide();
-
-						// Redirect.
-							window.setTimeout(function() {
-								window.location.href = href;
-							}, 350);
-
-					});
-
-			$body
-				.on('click', 'a[href="#menu"]', function(event) {
-
+				.on('click', function(event) {
 					event.stopPropagation();
-					event.preventDefault();
-
-					// Toggle.
-						$menu._toggle();
-
 				})
-				.on('keydown', function(event) {
+				.on('click', 'a', function(event) {
 
-					// Hide on escape.
-						if (event.keyCode == 27)
-							$menu._hide();
+					var href = $(this).attr('href');
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					// Hide.
+						$menu._hide();
+
+					// Redirect.
+						window.setTimeout(function() {
+							window.location.href = href;
+						}, 350);
 
 				});
 
-	});
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+			});
 
 })(jQuery);
