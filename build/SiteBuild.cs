a68962f1bd2da380077d0e1cd02cd5bb11616d2a
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-03-28 21:06:22 +0300

    Add projects structure

--- build/SiteBuild.cs
+++ build/SiteBuild.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetRu.Site.Generator;
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
@@ -11,136 +12,140 @@ using static Nuke.Common.ControlFlow;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Logger;
 
-[UnsetVisualStudioEnvironmentVariables]
-class SiteBuild : NukeBuild
+namespace DotNetRu.Site
 {
-    public static int Main () => Execute<SiteBuild>(build => build.UpdateSeeds);
-
-    [Parameter("News source address")]
-    readonly string NewsUrl;
-
-    [PathExecutable] readonly Tool PowerShell;
-    [PackageExecutable(
-        packageId: "Wyam",
-        packageExecutable: "Wyam.exe",
-        Version = "1.4.1")]
-    readonly Tool Wyam;
-
-    AbsolutePath InputDirectory => RootDirectory / "input";
-
-    AbsolutePath OutputDirectory => RootDirectory / "output";
-
-    AbsolutePath InputNewsDirectory => InputDirectory / "News";
-
-    AbsolutePath WyamConfigFile => RootDirectory / "config.wyam";
-
-    AbsolutePath SeedsDirectory => RootDirectory / ".." / "Seeds";
-
-    Target Dump => _ => _
-        .Executes(() =>
-        {
-            Info($"root: {RootDirectory}");
-            Info($"input: {InputDirectory}");
-            Info($"output: {OutputDirectory}");
-            Info($"wyam: {WyamConfigFile}");
-            Info($"seeds: {SeedsDirectory}");
-        });
-
-    Target Clean => _ => _
-        .Executes(() =>
-        {
-            EnsureCleanDirectory(OutputDirectory);
-        });
-
-    Target Build => _ => _
-        .DependsOn(Clean)
-        .DependsOn(Dump)
-        .Executes(() =>
-        {
-            Wyam($"--input \"{InputDirectory}\" --output \"{OutputDirectory}\" --config \"{WyamConfigFile}\" \"{RootDirectory}\"", workingDirectory: RootDirectory);
-        });
-
-    Target UpdateSeeds => _ => _
-        .DependsOn(Build)
-        .Executes(() =>
-        {
-            OutputDirectory.GlobDirectories("*").ForEach(UpdateSeed);
-            var statusFile = SeedsDirectory / "Get-SubGitStatus.ps1";
-            PowerShell($"-NoProfile -ExecutionPolicy Unrestricted -File \"{statusFile}\" -Path \"{SeedsDirectory}\"", workingDirectory: SeedsDirectory);
-
-            void UpdateSeed(AbsolutePath inputSeedDirectory)
-            {
-                var seedName = Path.GetFileName(inputSeedDirectory);
-                Info($"::::::::::::: {seedName} :::::::::::::");
-
-                var outputSeedDirectory = SeedsDirectory / seedName;
-                Assert(DirectoryExists(outputSeedDirectory), $"Output seed directory {outputSeedDirectory} not found. Run: Boombr Initialize-CommunityEnv");
-
-                CleanDirectory(outputSeedDirectory, ExceptGitDirectory);
-
-                Info($"Copy Seed directory: {inputSeedDirectory}");
-                CopyDirectoryRecursively(inputSeedDirectory, outputSeedDirectory, DirectoryExistsPolicy.Merge);
-            }
-
-            static bool ExceptGitDirectory(string path)
-            {
-                return
-                    !path.EndsWith("/.git") &&
-                    !path.Contains("/.git/") &&
-                    !path.EndsWith("\\.git") &&
-                    !path.Contains("\\.git\\");
-            }
-        });
-
-    void CleanDirectory(AbsolutePath directory, Func<string, bool> filter)
+    [UnsetVisualStudioEnvironmentVariables]
+    public sealed class SiteBuild : NukeBuild
     {
-        Info($"Cleaning directory '{directory} with filter'...");
+        public static int Main() => Execute<SiteBuild>(build => build.UpdateSeeds);
 
-        Directory
-            .GetFiles(directory)
-            .Where(filter)
-            .ForEach(DeleteFile);
+        [Parameter("News source address")] readonly string NewsUrl;
 
-        Directory
-            .GetDirectories(directory)
-            .Where(filter)
-            .ForEach(DeleteDirectory);
+        [PathExecutable] readonly Tool PowerShell;
+
+        [PackageExecutable(
+            packageId: "Wyam",
+            packageExecutable: "Wyam.exe",
+            Version = "1.4.1")]
+        readonly Tool Wyam;
+
+        AbsolutePath InputDirectory => RootDirectory / "input";
+
+        AbsolutePath OutputDirectory => RootDirectory / "output";
+
+        AbsolutePath InputNewsDirectory => InputDirectory / "News";
+
+        AbsolutePath WyamConfigFile => RootDirectory / "config.wyam";
+
+        AbsolutePath SeedsDirectory => RootDirectory / ".." / "Seeds";
+
+        Target Dump => _ => _
+            .Executes(() =>
+            {
+                Info($"root: {RootDirectory}");
+                Info($"input: {InputDirectory}");
+                Info($"output: {OutputDirectory}");
+                Info($"wyam: {WyamConfigFile}");
+                Info($"seeds: {SeedsDirectory}");
+            });
+
+        Target Clean => _ => _
+            .Executes(() =>
+            {
+                EnsureCleanDirectory(OutputDirectory);
+            });
+
+        Target Build => _ => _
+            .DependsOn(Clean)
+            .DependsOn(Dump)
+            .Executes(() =>
+            {
+                Wyam(
+                    $"--input \"{InputDirectory}\" --output \"{OutputDirectory}\" --config \"{WyamConfigFile}\" \"{RootDirectory}\"",
+                    workingDirectory: RootDirectory);
+            });
+
+        Target UpdateSeeds => _ => _
+            .DependsOn(Build)
+            .Executes(() =>
+            {
+                OutputDirectory.GlobDirectories("*").ForEach(UpdateSeed);
+                var statusFile = SeedsDirectory / "Get-SubGitStatus.ps1";
+                PowerShell(
+                    $"-NoProfile -ExecutionPolicy Unrestricted -File \"{statusFile}\" -Path \"{SeedsDirectory}\"",
+                    workingDirectory: SeedsDirectory);
+
+                void UpdateSeed(AbsolutePath inputSeedDirectory)
+                {
+                    var seedName = Path.GetFileName(inputSeedDirectory);
+                    Info($"::::::::::::: {seedName} :::::::::::::");
+
+                    var outputSeedDirectory = SeedsDirectory / seedName;
+                    Assert(DirectoryExists(outputSeedDirectory), $"Output seed directory {outputSeedDirectory} not found. Run: Boombr Initialize-CommunityEnv");
+
+                    CleanDirectory(outputSeedDirectory, ExceptGitDirectory);
+
+                    Info($"Copy Seed directory: {inputSeedDirectory}");
+                    CopyDirectoryRecursively(inputSeedDirectory, outputSeedDirectory, DirectoryExistsPolicy.Merge);
+                }
+
+                static bool ExceptGitDirectory(string path)
+                {
+                    return
+                        !path.EndsWith("/.git") &&
+                        !path.Contains("/.git/") &&
+                        !path.EndsWith("\\.git") &&
+                        !path.Contains("\\.git\\");
+                }
+            });
+
+        void CleanDirectory(AbsolutePath directory, Func<string, bool> filter)
+        {
+            Info($"Cleaning directory '{directory} with filter'...");
+
+            Directory
+                .GetFiles(directory)
+                .Where(filter)
+                .ForEach(DeleteFile);
+
+            Directory
+                .GetDirectories(directory)
+                .Where(filter)
+                .ForEach(DeleteDirectory);
+        }
+
+        Target News => _ => _
+            .Requires(() => NewsUrl)
+            .Executes(() =>
+            {
+                Info($"Creating News based on: {NewsUrl}");
+                var url = new Uri(NewsUrl);
+                var writer = new NewsWriter(InputNewsDirectory, Environment.UserName);
+
+                writer.BootstrapUrl(url);
+            });
+
+        Target Generate => _ => _
+            .DependsOn(Clean)
+            .ExecutesAsync(() =>
+            {
+                var args = new[]
+                {
+                    "--root", $"{RootDirectory}",
+                    "--input", $"{InputDirectory}",
+                    "--output", $"{OutputDirectory}",
+                    "--noclean"
+                };
+
+                return SiteGenerator.Main(args);
+            });
     }
 
-    Target News => _ => _
-        .Requires(() => NewsUrl)
-        .Executes(() =>
-        {
-            Info($"Creating News based on: {NewsUrl}");
-            var url = new Uri(NewsUrl);
-            var writer = new NewsWriter(InputNewsDirectory, Environment.UserName);
-
-            writer.BootstrapUrl(url);
-        });
-
-    Target Generate => _ => _
-        .DependsOn(Clean)
-        .ExecutesAsync(() =>
-        {
-            var args = new[]
-            {
-                "--root",
-                $"{RootDirectory}",
-                "--input",
-                $"{InputDirectory}",
-                "--output",
-                $"{OutputDirectory}",
-                "--noclean"
-            };
-
-            return SiteGenerate.Main(args);
-        });
-}
-
-public static class TargetDefinitionExtensions
-{
-    public static ITargetDefinition ExecutesAsync(this ITargetDefinition definition, Func<Task> action)
+    public static class TargetDefinitionExtensions
     {
-        return definition.Executes(() => action().GetAwaiter().GetResult());
+        public static ITargetDefinition ExecutesAsync(this ITargetDefinition definition, Func<Task> action)
+        {
+            return definition.Executes(() => action().GetAwaiter().GetResult());
+        }
     }
 }
