e4fdcfeffcac144d3431abfd4a76bb632f1a00d3
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-03-30 02:56:55 +0300

    Add Episode list pipeline

--- build/Generator/SiteGenerator.cs
+++ build/Generator/SiteGenerator.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.WebEncoders;
 using Statiq.App;
 using Statiq.Common;
-using Statiq.Core;
-using Statiq.Markdown;
-using Statiq.Yaml;
 
 namespace DotNetRu.Site.Generator
 {
@@ -17,23 +18,28 @@ namespace DotNetRu.Site.Generator
         //    [x] Convert Yaml to Metadata
         //    [ ] Use Razor with Layout
         //    [ ] Copy static files untouched
-        //    [ ] Add Index as output file
+        //    [x] Add Index as output file
         //    [ ] Add each episode as output file
+        //    [ ] Use design
         public static Task<int> Main(string[] args) => Bootstrapper
             .Factory
             .CreateDefault(args)
+            .ConfigureServices((services, settings) =>
+            {
+                services.Configure<WebEncoderOptions>(FixBasicLatinDefault);
+            })
             .AddSettings(new Dictionary<string, object>
             {
                 { Keys.Host, "radio.dotnet.ru" },
                 { Keys.LinksUseHttps, true },
                 { Keys.LinkLowercase, true }
             })
-            .BuildPipeline("Render Markdown", builder => builder
-                .WithInputReadFiles("Radio/*/*.md")
-                .WithProcessModules(
-                    new ExtractFrontMatter(new ParseYaml()),
-                    new RenderMarkdown())
-                .WithOutputWriteFiles(".html"))
             .RunAsync();
+
+        static void FixBasicLatinDefault(WebEncoderOptions options)
+        {
+            // https://github.com/aspnet/HttpAbstractions/issues/315
+            options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+        }
     }
 }
---
+++ build/Radio/EpisodeList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetRu.Site.Radio
+{
+    public sealed class EpisodeList
+    {
+        public EpisodeList(IReadOnlyList<Episode> episodes)
+        {
+            Episodes = episodes;
+        }
+
+        public IReadOnlyList<Episode> Episodes { get; }
+    }
+}
